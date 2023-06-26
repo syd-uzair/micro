9351ed0d88288f1b8e3fc0d9564c711c1c7c26cb
syd-uzair <syd.uzairahmed@gmail.com>
2023-06-26 14:37:18 +0100

    add code

---
+++ service/src/test/java/micro/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
